1a983bc6c66b85add60d83a609ea13f9a2a112d4
Paulemberg L. Silva <paulemberg@gmail.com>
2018-12-16 01:30:47 -0200

    adicionando arquivos e pasta removdia

---
+++ SampleAPIWPF/SunInfo.xaml.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary;
+using System.Windows;
+
+namespace SampleAPIWPF
+{
+    /// <summary>
+    /// Lógica interna para SunInfo.xaml
+    /// </summary>
+    public partial class SunInfo : Window
+    {
+        public SunInfo()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadSunInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var sunInfo = await SunProcessor.LoadSunInformation();
+            sunriseText.Text = $"Sunrise is at {sunInfo.Sunrise.ToLocalTime().ToShortTimeString()}";
+            sunsetText.Text = $"Sunset is at {sunInfo.Sunset.ToLocalTime().ToShortTimeString()}";
+        }
+    
+    }
+}
